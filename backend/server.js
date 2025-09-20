964aab1e27bf2b2c700915c762fd3a3c9976eb6a
jeshwitha <ajeshwitha@gmail.com>
2025-09-20 15:43:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const chatRoutes = require('./routes/chatRoutes')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(express.json())
